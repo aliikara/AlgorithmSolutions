f42521a07fdf34b531803c3275653d27fcb3d869
ALİ KARA <disconnectfromserver19@gmail.com>
2024-01-26 22:29:56 +0300

    Example

---
+++ SumOfSquaresOfTwoNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿//Console.WriteLine("1. sayıyı giriniz : ");
+//int sayi1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("2. sayıyı giriniz : ");
+//int sayi2 = int.Parse(Console.ReadLine());
+
+//int sonuc = (sayi1 * sayi1) + (sayi2 * sayi2);
+//double sonuc = Math.Pow(sayi1, 2) + Math.Pow(sayi2, 2);
+
+Console.WriteLine("1. ve 2. sayıları giriniz.");
+double sonuc = Math.Pow(int.Parse(Console.ReadLine()), 2) + Math.Pow(int.Parse(Console.ReadLine()), 2);
+Console.WriteLine(sonuc);
---
+++ TheSumOfTheCubesOfTheNumbersOneThroughTen/Program.cs
@@ -0,0 +1,18 @@
+﻿//double toplamSonuc = 0;
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    toplamSonuc += Math.Pow(i, 3);
+//}
+//Console.WriteLine(toplamSonuc);
+
+double toplamSonuc = 0;
+int sayac = 1;
+while (true)
+{
+    toplamSonuc += Math.Pow(sayac, 3);
+    if (sayac == 10) break;
+    sayac++;
+
+}
+Console.WriteLine(toplamSonuc);
