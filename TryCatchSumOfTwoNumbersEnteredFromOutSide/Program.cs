209bfe954f7f736370a8a2cbf394c3da5ebc01ab
ALİ KARA <disconnectfromserver19@gmail.com>
2024-01-26 21:50:50 +0300

    Example

---
+++ TryCatchSumOfTwoNumbersEnteredFromOutSide/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    Console.WriteLine("1. Sayıyı Giriniz : ");
+    int sayi1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("2. Sayıyı Giriniz : ");
+    int sayi2 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(sayi1 + sayi2);
+}
+catch 
+{
+    Console.WriteLine("Lütfen doğru bir sayı giriniz.");
+
+}
