0f2a91876bf9e6204f00b50d0a708fbfe59e952f
ALİ KARA <disconnectfromserver19@gmail.com>
2024-01-27 13:39:16 +0300

    Example

---
+++ AgeCalculation/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Doğum tarihinizi giriniz");
+
+//DateTime dogumTarihi = Convert.ToDateTime(Console.ReadLine());
+//TimeSpan sonuc = DateTime.Now - dogumTarihi;
+//Console.WriteLine(sonuc.Days / 365);
+
+//DateTime dogumTarihi = DateTime.Parse(Console.ReadLine());
+//DateTime bugun = DateTime.Now;
+//int yas = bugun.Year - dogumTarihi.Year;
+//if (dogumTarihi > bugun.AddYears(-yas))
+//    yas--;
+//Console.WriteLine(yas);
+
+DateTime dogumTarihi = DateTime.Parse(Console.ReadLine());
+int gun = (DateTime.Now - dogumTarihi).Days;
+int yas = gun / 365;
+int kalan = gun % 365;
+Console.WriteLine($"Yaş : {yas} | {yas + 1} yaşınıza kalan gün sayısı : {365 + (yas * 1 / 4) - kalan}");
--- AgeCalculation/obj/Debug/net8.0/AgeCalculation.AssemblyInfo.cs
+++ AgeCalculation/obj/Debug/net8.0/AgeCalculation.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TheSumOfTheCubesOfTheNumbersOneThroughTen")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("AgeCalculation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("TheSumOfTheCubesOfTheNumbersOneThroughTen")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TheSumOfTheCubesOfTheNumbersOneThroughTen")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f42521a07fdf34b531803c3275653d27fcb3d869")]
+[assembly: System.Reflection.AssemblyProductAttribute("AgeCalculation")]
+[assembly: System.Reflection.AssemblyTitleAttribute("AgeCalculation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
